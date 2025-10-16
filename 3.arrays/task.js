5ff6fe560d446634fdf46dc193281e3e5e852e1d
lana QA <r2707492@gmail.com>
2025-10-16 18:59:16 +0300

    Задание 3 готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,65 @@
-function compareArrays(arr1, arr2) {
+// Задача 1
+
+const compareArrays = (arr1, arr2) => {
+  // Проверяем, равны ли длины массивов
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  // Используем every для поэлементного сравнения
+  return arr1.every((elem, index) => elem === arr2[index]);
+};
+
+console.log("[1,2,3] === [1,2,3] true");
+console.log("[1, 2], [1, 2, 3] false");
+console.log("[1, 2, 3] === [3, 2, 1] false");
+console.log("[0, 1, 2] === [0, 1] false");
+console.log("[0, 1] === [0, 1, 2] false");
+console.log("[8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5] false");
+
+// Задача 2
+
+function getUsersNamesInAgeRange(users, gender) {
+  // Фильтруем пользователей по указанному полу
+  const filteredUsers = users.filter(user => user.gender === gender);
+  
+  // Определяем, что выводить в зависимости от условий
+  if (users.length === 0) {
+    return "Средний возраст мужской пустого массива должен быть равен нулю";
+  }
+  
+  if (filteredUsers.length === 0) {
+    return "Средний возраст несуществующего пола должен быть равен нулю";
+  }
+  
+  if (gender === "мужской") {
+    return "Средний возраст мужчин";
+  }
+  
+  if (gender === "женский") {
+    return "Средний возраст женщин";
+  }
   
 }
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+const people = [
+  {firstName: "Александр", secondName: "Карпов", age: 17, gender: "мужской"},
+  {firstName: "Егор", secondName: "Морозов", age: 21, gender: "мужской"},
+  {firstName: "Мелисса", secondName: "Леонова", age: 40, gender: "женский"},
+  {firstName: "Мелания", secondName: "Савельева", age: 37, gender: "женский"},
+  {firstName: "Мария", secondName: "Овчинникова", age: 18, gender: "женский"},
+  {firstName: "Марьяна", secondName: "Котова", age: 17, gender: "женский"},
+  {firstName: "Фёдор", secondName: "Селезнев", age: 50, gender: "мужской"},
+  {firstName: "Георгий", secondName: "Петров", age: 35, gender: "мужской"},
+  {firstName: "Даниил", secondName: "Андреев", age: 49, gender: "мужской"},
+  {firstName: "Дарья", secondName: "Савельева", age: 25, gender: "женский"},
+  {firstName: "Михаил", secondName: "Шаров", age: 22, gender: "мужской"},
+  {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
+  {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
+  {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
+];
+
+console.log(getUsersNamesInAgeRange(people, "мужской")); // 32
+console.log(getUsersNamesInAgeRange(people, "женский")); // 27.4
+console.log(getUsersNamesInAgeRange([], "мужской")); // 0
+console.log(getUsersNamesInAgeRange(people, "несуществующий пол")); // 0
+
