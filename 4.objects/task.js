141e988f16f0ed69e61b5f4b1f83c7705a5eef1a
lana QA <r2707492@gmail.com>
2025-10-17 18:01:55 +0300

    Задание выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -17,3 +17,62 @@ Student.prototype.getAverage = function () {
 Student.prototype.exclude = function (reason) {
   
 }
+
+
+
+// Функция-конструктор Student
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = []; // Пустой массив для оценок
+}
+
+// Метод setSubject: устанавливает предмет
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
+};
+
+// Метод addMarks: добавляет несколько оценок с проверкой
+Student.prototype.addMarks = function(...marksToAdd) {
+  if (this.marks) { // Проверяем, существует ли marks (если студент не отчислен)
+    this.marks.push(...marksToAdd);
+  }
+};
+
+// Метод getAverage: возвращает средний балл с проверкой
+Student.prototype.getAverage = function() {
+  if (!this.marks || this.marks.length === 0) {
+    return 0;
+  }
+  const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+  return sum / this.marks.length;
+};
+
+// Метод exclude: исключает студента и устанавливает причину
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
+
+// Использование
+let student1 = new Student("Елена", "женский", 18);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+
+let student2 = new Student("Николай", "мужской", 21);
+student2.setSubject("Physics");
+student2.exclude('плохая учёба');
+console.log(student2);
+
+let student3 = new Student("Андрей", "мужской", 23);
+student3.setSubject("Сhemistry");
+console.log(student3.getAverage()); // 0
+student3.addMarks(3, 4, 3, 4);
+console.log(student3.getAverage()); // 4.5
+console.log(student3);
+
